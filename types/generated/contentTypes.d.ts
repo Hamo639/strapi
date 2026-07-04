1eccb6d9effad8c5213b0a1d618d880bcf52b6dc
Hamo639 <116502711+Hamo639@users.noreply.github.com>
2026-07-04 08:17:11 +0300

    Update3

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -538,6 +538,10 @@ export interface ApiOrderOrder extends Struct.CollectionTypeSchema {
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    user: Schema.Attribute.Relation<
+      'manyToOne',
+      'plugin::users-permissions.user'
+    >;
   };
 }
 
@@ -1079,6 +1083,7 @@ export interface PluginUsersPermissionsUser
       'plugin::users-permissions.user'
     > &
       Schema.Attribute.Private;
+    orders: Schema.Attribute.Relation<'oneToMany', 'api::order.order'>;
     password: Schema.Attribute.Password &
       Schema.Attribute.Private &
       Schema.Attribute.SetMinMaxLength<{
